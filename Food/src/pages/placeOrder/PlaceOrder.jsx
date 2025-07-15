244c810f958d3c7588bd09e0a6c6d77aef8c18d8
kireeti-ai <kireetiv2005@gmail.com>
2025-07-15 18:27:40 +0530

    few changes

--- Food/src/pages/placeOrder/PlaceOrder.jsx
+++ Food/src/pages/placeOrder/PlaceOrder.jsx
@@ -1,6 +1,6 @@
 
 import React, { useContext } from 'react';
-import './PlaceOrder.css';
+import './placeOrder.css';
 import { StoreContext } from '../../Context/StoreContext';
 
 const PlaceOrder = () => {
