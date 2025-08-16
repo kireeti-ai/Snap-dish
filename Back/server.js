95491bed7f1492f0e4e8219d37c405437845b81a
Kireeti <kireetiv2005@gmail.com>
2025-08-16 15:46:24 +0530

    user

--- Back/server.js
+++ Back/server.js
@@ -1,39 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
-import { connectDB } from "./config/db.js"; // Assuming you have this file
+import { connectDB } from "./config/db.js";
 
-// --- Import Routes ---
 import userRoutes from "./routes/userRoutes.js";
 import restaurantRoutes from "./routes/restaurantRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
 import deliveryAgentRoutes from "./routes/deliveryAgentRoutes.js";
 
-// --- App Config ---
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
 // --- Middleware ---
 app.use(express.json()); // To parse JSON bodies
-const allowedOrigins = [
-  "http://localhost:5173",          // for local dev
-  "https://snap-dish-xi.vercel.app" // your deployed frontend
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // if you’re using cookies/auth
-  })
-); // To allow cross-origin requests
+app.use(cors({ origin: "*" })); // To allow cross-origin requests
 
 // --- DB Connection ---
 connectDB();
