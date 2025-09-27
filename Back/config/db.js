3ecc65b794c9c33f46b1868b44b90945b53c2672
Kireeti <kireetiv2005@gmail.com>
2025-09-27 12:22:30 +0530

    .

--- Back/config/db.js
+++ Back/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://kireeti:kireetiv123@cluster0.cdfmllx.mongodb.net/snap-dish').then(()=>console.log("DB connected"))
+    await mongoose.connect('mongodb+srv://admin:kireeti@cluster0.cdfmllx.mongodb.net/snap-dish').then(()=>console.log("DB connected"))
 
 }
